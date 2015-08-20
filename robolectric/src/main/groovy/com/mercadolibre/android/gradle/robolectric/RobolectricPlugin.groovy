618423cc21095c27038a86c1d1d40a0fd80a2e47
Juan Martín Sotuyo Dodero <jmsotuyo@monits.com>
2015-08-20 17:01:43 -0300

    Make plugins compatible with Android plugin 1.3
     - The release 1.3 of the android gradle plugin introduced changes
        to the name of a few tasks that caused NPEs when using it.
     - This fix works with all versions, by detecting the task to be used.
     - This fixes #26

--- robolectric/src/main/groovy/com/mercadolibre/android/gradle/robolectric/RobolectricPlugin.groovy
+++ robolectric/src/main/groovy/com/mercadolibre/android/gradle/robolectric/RobolectricPlugin.groovy
@@ -49,6 +49,13 @@ public class RobolectricPlugin implements Plugin<Project> {
         variants.all { variant ->
             def taskName = "test${variant.flavorName.capitalize()}${variant.buildType.name.capitalize()}"
             def testTask = project.tasks.findByName(taskName)
+
+            //Forward compatibility to android grdle plugin 1.3.+
+            if (testTask == null) {
+                taskName += "UnitTest"
+                testTask = project.tasks.findByName(taskName)
+            }
+
             testTask.dependsOn("createRobolectricFiles")
         }
     }
