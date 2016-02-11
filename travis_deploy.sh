be4c5cbd0d73579c2efa9321f94ec415343dc9dc
Nicolas Suarez <nicolasandres.suarez@mercadolibre.com>
2016-02-11 16:06:24 -0300

    Download Rakefile from master branch [ci deploy]

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -2,6 +2,7 @@
 
 curl -H "Authorization: token ${GITHUB_TOKEN}" \
   -H "Accept: application/vnd.github.v3.raw" \
-  -O -L https://api.github.com/repos/mercadolibre/mobile-cd/contents/android/Rakefile
+  -L https://api.github.com/repos/mercadolibre/mobile-cd/contents/android/Rakefile?ref=master \
+  -o Rakefile
 
 rake release['base application library jacoco robolectric',bintray]
