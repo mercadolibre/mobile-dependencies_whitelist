ca893fac7a6057f8e95807f3c6b2ba1a4ee7ac93
saantiaguilera <santiago.aguilera@mercadolibre.com>
2017-07-26 21:05:04 -0300

    Fix version mismatch for timestamped versions

--- base/src/main/groovy/com/mercadolibre/android/gradle/base/utils/VersionContainer.groovy
+++ base/src/main/groovy/com/mercadolibre/android/gradle/base/utils/VersionContainer.groovy
@@ -8,7 +8,9 @@ final class VersionContainer {
     private static final Map<String, String> map = new HashMap<>()
 
     public static void put(String publicationName, String version) {
-        map.put(publicationName, version)
+        if (!map.get(publicationName)) {
+            map.put(publicationName, version)
+        }
     }
 
     public static String get(String publicationName, String defaultValue = '') {
