82690d4addc2d6a63243065d41993b9b3c5fe645
Martin Feldsztejn <mfeldsztejn@gmail.com>
2017-08-16 10:40:36 -0300

    Revert dependsOn jar

--- base/src/main/groovy/com/mercadolibre/android/gradle/base/publish/PublishJarExperimentalTask.groovy
+++ base/src/main/groovy/com/mercadolibre/android/gradle/base/publish/PublishJarExperimentalTask.groovy
@@ -30,7 +30,7 @@ class PublishJarExperimentalTask extends PublishJarTask {
                 }
                 group = TASK_GROUP
 
-                dependsOn "${variant.name}JavadocJar"
+                dependsOn "jar", "${variant.name}JavadocJar"
                 finalizedBy 'bintrayUpload'
             }
         }
