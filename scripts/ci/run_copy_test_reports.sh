ad58155621e6862b407dd4a2a3ab16f398a973c1
saantiaguilera <aguilerasanti@hotmail.com>
2018-02-14 15:28:38 -0300

    Migrate to kelli

--- scripts/ci/run_copy_test_reports.sh
+++
@@ -1,8 +0,0 @@
-mkdir -p tmp/build/reports
-mkdir -p tmp/build/test-results
-if [ -d "$CIRCLE_JOB/build/reports" ]; then
-    cp -r $CIRCLE_JOB/build/reports tmp/build/reports
-fi
-if [ -d "$CIRCLE_JOB/build/test-results" ]; then
-    cp -r $CIRCLE_JOB/build/test-results tmp/build/test-results
-fi
--- scripts/ci/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-set -o pipefail
-
-source mobile-cd/scripts/base_script.sh
-./gradlew -Dorg.gradle.daemon=true $CIRCLE_JOB$TEST_TASK -PdisablePreDex | sed -f "$STDOUT_FILTERS"
-f_exit_on_error ""
