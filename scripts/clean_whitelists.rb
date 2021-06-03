e6c76abba2c1186d8ede363777a2dd824b176fa1
Bruno Ardanaz <bruno.ardanaz@mercadolibre.com>
2021-06-03 16:09:18 -0300

    changes

--- scripts/clean_whitelists.rb
+++ scripts/clean_whitelists.rb
@@ -11,23 +11,28 @@ module Clean_whitelists
         return JSON.parse(file)
     end
 
-	def self.save_json_to_file(hashData, pathFile)
-		File.open(pathFile,"w") do |f|
-      		f.write(JSON.pretty_generate(hashData))
-		end
-	end
+    def self.save_json_to_file(hashData, pathFile)
+        File.open(pathFile,"w") do |f|
+            f.write(JSON.pretty_generate(hashData))
+        end
+    end
 
-	def self.removeExpired(hashDataList)
-		listCopy = hashDataList
-		hashDataList["whitelist"].each_entry do |entry, v|
-			if entry.key?("expires")
-				if is_expired(entry["expires"])
-					listCopy["whitelist"].delete(entry)
-				end
-			end
-		end
-		return listCopy
-	end
+    def self.removeExpired(hashDataList)
+        #listCopy = {}
+        listCopy = hashDataList
+        #listCopy["whitelist"]
+        hashDataList["whitelist"].each_entry do |entry, v|
+            if entry.key?("expires") && is_expired(entry["expires"])
+                    listCopy["whitelist"].delete(entry)
+            end
+            #if !entry.key?("expires") or !is_expired(entry["expires"])
+            #    puts "something"
+	        #    listCopy["whitelist"].add(entry)
+			#end
+        end
+        #puts listCopy
+        return listCopy
+    end
 
     def self.is_expired(expireDate)
         Date.parse(expireDate) < Date.today
@@ -35,43 +40,52 @@ module Clean_whitelists
 
     def self.main()
         puts "can execute main"
-		dataHashAndroid = get_json_from_file(ANDROID_WHITELIST_PATH_FILE)
-		dataHashIos = get_json_from_file(IOS_WHITELIST_PATH_FILE)
+        dataHashAndroid = get_json_from_file(ANDROID_WHITELIST_PATH_FILE)
+        dataHashIos = get_json_from_file(IOS_WHITELIST_PATH_FILE)
 
-		cleanHash = removeExpired(dataHashAndroid)
-		save_json_to_file(cleanHash, "pp.txt")
-		cleanHash = removeExpired(dataHashIos)
-		save_json_to_file(cleanHash, "ppios.txt")
+        cleanHash = removeExpired(dataHashAndroid)
+        puts "Size cleanHash: " + cleanHash["whitelist"].size.to_s
+        puts "Size dataHashAndroid: " + dataHashAndroid["whitelist"].size.to_s
+        puts cleanHash["whitelist"].to_a == dataHashAndroid["whitelist"].to_a
 
-		puts 'git commit -am "remove expired libs"'
-		create_pr()
+        save_json_to_file(cleanHash, "pp.txt")
+
+        cleanHash = removeExpired(dataHashIos)
+        save_json_to_file(cleanHash, "ppios.txt")
+
+        #create_pr()
     end
 
-	def self.create_pr()
-		puts "\nCreating pull request " + Date.today.to_s()
-		#os.chdir(self.repo_path)
-		prBranchName = "fix/cleanExpiredLibs/" + Date.today.to_s()
+    def self.create_pr()
+    	currentDate = Date.today.to_s()
+        puts "\nCreating pull request " + currentDate
+        #os.chdir(self.repo_path)
+        prBranchName = "fix/cleanExpiredLibs/" + currentDate
 
-		res = system("git checkout -b " + prBranchName) #+ " --quiet >/dev/null 2>&1")
-		res = system('git commit -am "remove expired libs"') #+ " --quiet >/dev/null 2>&1")
-		res = system("git push origin " + prBranchName) #+ " --quiet >/dev/null 2>&1")
+        res = system("git checkout -b " + prBranchName) #+ " --quiet >/dev/null 2>&1")
+        res = system('git commit -am "remove expired libs until: ' +currentDate + '"') #+ " --quiet >/dev/null 2>&1")
+        res = system("git push origin " + prBranchName) #+ " --quiet >/dev/null 2>&1")
 
-		url = "https://github.com/mercadolibre/mobile-dependencies_whitelist/pulls"
-		uri = URI.parse(url)
-		header = {'Content-Type': 'application/vnd.github.v3+json'}
-		# Create the HTTP objects
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.use_ssl = true
-		request = Net::HTTP::Post.new(uri.request_uri, header)
-		request.body = {
-			title: "[Trivial] Clean old expired libs",
-			head: PR_BRANCH_NAME,
-			base: "master",
-			body: "This Pull Request deletes expired libs"
-		}.to_json
-		# response = http.request(request)
+        url = "https://github.com/mercadolibre/mobile-dependencies_whitelist/pulls"
+        uri = URI.parse(url)
 
-	end
+        header = {'Content-Type': 'application/json',
+			'Accept': 'application/vnd.github.v3+json',
+			'Authorization': ENV["GITHUB_TOKEN"]
+        }
+        # Create the HTTP objects
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        request = Net::HTTP::Post.new(uri.request_uri, header)
+        request.body = {
+            title: "[Trivial] Clean old expired libs",
+            head: PR_BRANCH_NAME,
+            base: "master",
+            body: "This Pull Request deletes expired libs"
+        }.to_json
+        response = http.request(request)
 
-    main()
+    end
+
+    #main()
 end
