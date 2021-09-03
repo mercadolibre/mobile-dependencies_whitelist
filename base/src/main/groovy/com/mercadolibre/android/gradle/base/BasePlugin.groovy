882288c956568c065f1aedf1194f7d5d02f976a0
fllallire <fernando.llallire@mercadolibre.com>
2021-09-03 18:48:50 -0300

    change in all projects configuration

--- base/src/main/groovy/com/mercadolibre/android/gradle/base/BasePlugin.groovy
+++ base/src/main/groovy/com/mercadolibre/android/gradle/base/BasePlugin.groovy
@@ -314,7 +314,7 @@ class BasePlugin implements Plugin<Object> {
      * to allow the android environment
      */
     private void removeMavenRepository(){
-        project.repositories.removeAll {
+        project.repositories {
             it instanceof MavenArtifactRepository && it.url.toString() == "https://maven.artifacts.furycloud.io"
         }
     }
