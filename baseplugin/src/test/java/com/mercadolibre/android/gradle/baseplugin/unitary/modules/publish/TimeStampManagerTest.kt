5050e25c3b009f8c50e6e8d10fd48ceb31d17ba2
Matias Manzanzani <matias.manzanzani@mercadolibre.com>
2022-07-15 15:49:32 -0300

    test-upgrade

--- baseplugin/src/test/java/com/mercadolibre/android/gradle/baseplugin/unitary/modules/publish/TimeStampManagerTest.kt
+++ baseplugin/src/test/java/com/mercadolibre/android/gradle/baseplugin/unitary/modules/publish/TimeStampManagerTest.kt
@@ -1,9 +1,17 @@
 package com.mercadolibre.android.gradle.baseplugin.unitary.modules.publish
 
 import com.mercadolibre.android.gradle.baseplugin.core.action.modules.publishable.basics.TimeStampManager
+import com.mercadolibre.android.gradle.baseplugin.core.components.PUBLISHING_TIME_GENERATOR
+import com.mercadolibre.android.gradle.baseplugin.core.components.PUBLISHING_TIME_ZONE
 import com.mercadolibre.android.gradle.baseplugin.managers.AbstractPluginManager
 import com.mercadolibre.android.gradle.baseplugin.managers.LIBRARY_PROJECT
 import com.mercadolibre.android.gradle.baseplugin.managers.ROOT_PROJECT
+import io.mockk.every
+import io.mockk.mockkClass
+import io.mockk.mockkConstructor
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.TimeZone
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -20,18 +28,24 @@ class TimeStampManagerTest : AbstractPluginManager() {
 
     @org.junit.Test
     fun `Get a time stamp`() {
-        TimeStampManager.getOrCreateTimeStamp() // Create
-        TimeStampManager.getOrCreateTimeStamp() // Get
+        val createdTime = TimeStampManager.getOrCreateTimeStamp() // Create
+
+        assert(createdTime === TimeStampManager.getOrCreateTimeStamp()) // Get
     }
 
     @org.junit.Test
     fun `Delete a time stamp`() {
-        TimeStampManager.getOrCreateTimeStamp() // Create
+        val createdTime = TimeStampManager.getOrCreateTimeStamp() // Create
+
         TimeStampManager.deleteTimeStamp() // Delete
+
+        assert(TimeStampManager.getOrCreateTimeStamp() !== createdTime)
     }
 
     @org.junit.Test
     fun `Delete a time stamp if not exist`() {
         TimeStampManager.deleteTimeStamp() // Delete
+        assert(TimeStampManager.getOrCreateTimeStamp() == SimpleDateFormat(PUBLISHING_TIME_GENERATOR)
+            .apply { timeZone = TimeZone.getTimeZone(PUBLISHING_TIME_ZONE) }.format(Date()))
     }
 }
