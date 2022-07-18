f26789fc4dc0eb8fe65ad0546fecb2a6d7b2f5dc
Matias Manzanzani <matias.manzanzani@mercadolibre.com>
2022-07-18 10:45:12 -0300

    Docu

--- baseplugin/src/main/java/com/mercadolibre/android/gradle/baseplugin/core/action/modules/publishable/basics/TimeStampManager.kt
+++ baseplugin/src/main/java/com/mercadolibre/android/gradle/baseplugin/core/action/modules/publishable/basics/TimeStampManager.kt
@@ -6,17 +6,28 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.TimeZone
 
+/**
+ * This class is in charge of generating a TimeStamp for all publication tasks, as long as they are the same.
+ */
 object TimeStampManager {
 
     private var time: String? = null
 
+    /**
+     * This method generates a time stamp the first time it is called and then returns the same object.
+     */
     fun getOrCreateTimeStamp(): String {
         if (time == null) {
-            time = SimpleDateFormat(PUBLISHING_TIME_GENERATOR).apply { timeZone = TimeZone.getTimeZone(PUBLISHING_TIME_ZONE) }.format(Date())
+            time = SimpleDateFormat(PUBLISHING_TIME_GENERATOR)
+                .apply { timeZone = TimeZone.getTimeZone(PUBLISHING_TIME_ZONE) }
+                .format(Date())
         }
         return time!!
     }
 
+    /**
+     * This method removes the previously generated timestamp.
+     */
     fun deleteTimeStamp() {
         time = null
     }
