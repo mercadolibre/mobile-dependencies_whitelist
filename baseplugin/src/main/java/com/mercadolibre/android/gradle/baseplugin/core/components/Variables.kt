fb6c3d640fe621ca0fffaecbe6db37d5e0eb8ee9
Matias Manzanzani <matias.manzanzani@mercadolibre.com>
2022-09-01 15:58:50 -0300

    fix

--- baseplugin/src/main/java/com/mercadolibre/android/gradle/baseplugin/core/components/Variables.kt
+++ baseplugin/src/main/java/com/mercadolibre/android/gradle/baseplugin/core/components/Variables.kt
@@ -9,6 +9,3 @@ internal const val BUILD_TOOLS_VERSION = "30.0.2"
 internal const val GRADLE_VERSION = 7
 
 internal val JAVA_VERSION = JavaVersion.VERSION_1_8
-
-internal val ANDROID_LIBRARY_PLUGIN = "com.android.library"
-internal val ANDROID_APPLICATION_PLUGIN = "com.android.application"
